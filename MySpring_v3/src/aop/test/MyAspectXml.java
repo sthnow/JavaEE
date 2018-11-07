902590e782fa969c2ec66388deb8ffc499bbec16
wzz <568780720@qq.com>
2018-11-07 23:36:31 +0800

    用注解的方式使用aop

--- MySpring_v3/src/aop/test/MyAspectXml.java
+++ MySpring_v3/src/aop/test/MyAspectXml.java
@@ -1,11 +1,19 @@
 package aop.test;
 
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Value;
+
 //切面类
+//切面类的注解
+//在注解中表明切面类
+@Aspect
 public class MyAspectXml {
 
 
-    //通知
+    //通过注解选择通知的织入方式和设置切入点
+    @Before(value = "execution(public void aop.test.OrderDaoImpl.save())")
     public void before(){
         System.out.println("前置增强");
     }
