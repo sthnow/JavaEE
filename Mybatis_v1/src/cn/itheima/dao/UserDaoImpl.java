7283806dda1d4f885dc53d4be32bc4edba6f0fcb
wzz <568780720@qq.com>
2018-11-19 23:47:44 +0800

    更新

--- Mybatis_v1/src/cn/itheima/dao/UserDaoImpl.java
+++ Mybatis_v1/src/cn/itheima/dao/UserDaoImpl.java
@@ -10,25 +10,27 @@ import cn.itheima.pojo.User;
 public class UserDaoImpl implements UserDao {
 
 	private SqlSessionFactory sqlSessionFactory;
+	private SqlSession openSession;
 
 	//通过构造方法注入
 	public UserDaoImpl(SqlSessionFactory sqlSessionFactory) {
 		this.sqlSessionFactory = sqlSessionFactory;
 	}
-
+	public UserDaoImpl(SqlSession sqlSession){this.openSession = sqlSession;}
 		@Override
 		public User findUserById(Integer id) {
 			//sqlSesion是线程不安全的,所以它的最佳使用范围在方法体内
-			SqlSession openSession = sqlSessionFactory.openSession();
+		//	SqlSession openSession = sqlSessionFactory.openSession();
 		User user = openSession.selectOne("test.findUserById", id);
 		return user;
 	}
 
 	@Override
 	public List<User> findUserByUserName(String userName) {
-		SqlSession openSession = sqlSessionFactory.openSession();
+		//SqlSession openSession = sqlSessionFactory.openSession();
 		List<User> list = openSession.selectList("test.findUserByUserName", userName);
 		return list;
+		
 	}
 	
 	
