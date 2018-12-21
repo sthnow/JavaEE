9fb09d76b4289e4dd2c4c9559969ae829f93e803
wangzh <568780720@qq.com>
2018-12-22 00:14:40 +0800

    DAO层，接口和mapper文件

---
+++ ssm-crm/src/cn/itheima/dao/CustomerMapper.java
@@ -0,0 +1,4 @@
+package cn.itheima.dao;
+
+public class CustomerMapper {
+}
---
+++ ssm-crm/src/cn/itheima/dao/DictMapper.java
@@ -0,0 +1,10 @@
+package cn.itheima.dao;
+
+import cn.itheima.pojo.BaseDict;
+
+import java.util.List;
+
+public interface DictMapper {
+
+    public List<BaseDict> findDictById(String code);
+}
